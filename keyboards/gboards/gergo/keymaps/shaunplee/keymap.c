d36d87959cbb6bc7fed4d21cf5993c51a7ea0494
Shaun Lee <shaun.lee@gmail.com>
2023-10-31 22:17:45 -0700

    Change symbol modifier key to one shot layer.

--- keyboards/gboards/gergo/keymaps/shaunplee/keymap.c
+++ keyboards/gboards/gergo/keymaps/shaunplee/keymap.c
@@ -48,7 +48,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     LT(NUMPAD, KC_TAB),     KC_Q,  KC_W,   KC_E,   KC_R, KC_T,                                             KC_Y,    KC_U, KC_I, KC_O,   KC_P,    KC_LBRC,
     KC_LCTL,                KC_A,  KC_S,   KC_D,   KC_F, KC_G, MO(MOUSE),                        KC_INS,   KC_H,    KC_J, KC_K, KC_L,   KC_SCLN, RCTL_T(KC_QUOT),
     KC_LSFT,                KC_Z,  KC_X,   KC_C,   KC_V, KC_B, KC_ESC, KC_APP,       TG(NUMPAD), KC_DEL,   KC_N,    KC_M, KC_COMM, KC_DOT, KC_SLSH, RSFT_T(KC_PIPE),
-                           KC_LGUI, MO(SYMB), LT(NUMB, KC_SPC), KC_LALT,               KC_BSPC,    LT(NUMB, KC_ENT), MO(SYMB), KC_RCTL
+                           KC_LGUI, OSL(SYMB), LT(NUMB, KC_SPC), KC_LALT,               KC_BSPC,    LT(NUMB, KC_ENT), OSL(SYMB), KC_RCTL
     ),
 /* Keymap 1: Dvorak layer
  *
@@ -74,7 +74,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     LT(NUMPAD, KC_TAB), KC_QUOT,  KC_COMM,KC_DOT, KC_P, KC_Y,                                             KC_F,  KC_G, KC_C, KC_R, KC_L, KC_SLASH,
     KC_LCTL,            KC_A,     KC_O,   KC_E,   KC_U, KC_I, MO(MOUSE),                        KC_INS,   KC_D,  KC_H, KC_T, KC_N, KC_S, RCTL_T(KC_MINUS),
     KC_LSFT,            KC_SCLN,KC_Q,   KC_J,   KC_K, KC_X, KC_ESC, KC_APP,       TG(NUMPAD), KC_DEL,   KC_B,  KC_M, KC_W, KC_V, KC_Z, RSFT_T(KC_PIPE),
-                        KC_LGUI, MO(SYMB), LT(NUMB, KC_SPC), KC_LALT,               KC_BSPC,    LT(NUMB, KC_ENT), MO(SYMB), KC_RCTL
+                        KC_LGUI, OSL(SYMB), LT(NUMB, KC_SPC), KC_LALT,               KC_BSPC,    LT(NUMB, KC_ENT), OSL(SYMB), KC_RCTL
     ),
 /* Keymap 2: Symbols layer
  *
